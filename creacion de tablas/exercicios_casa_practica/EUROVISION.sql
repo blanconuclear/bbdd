c18d1f5843c23ff6a75975518d50c5b9b9ae0564
Efrén Corzón Vázquez <efrencorzonvazquez@Mac-mini-de-Efren.local>
2024-02-13 20:47:22 +0100

    practica

---
+++ creacion de tablas/exercicios_casa_practica/EUROVISION.sql
@@ -0,0 +1,67 @@
+-- Verifica si la base de datos EUROVISION existe, si sí, bórrala
+IF EXISTS (SELECT * FROM sys.databases WHERE name = 'EUROVISION')
+BEGIN
+    DROP DATABASE EUROVISION;
+END
+GO
+
+-- Crea la base de datos EUROVISION
+CREATE DATABASE EUROVISION;
+GO
+
+-- Crea la tabla INTERPRETE
+CREATE TABLE INTERPRETE(
+    numero int NOT NULL,
+    nombre VARCHAR(30) NOT NULL,
+    fecha DATE NULL,
+    num_cancion INT NOT NULL,
+
+    CONSTRAINT PK_INTERPRETE_numero PRIMARY KEY (numero),
+    CONSTRAINT UQ_INTERPRETE_nombre UNIQUE (nombre),
+    -- El nombre de la tabla referenciada debe ser CANCION en lugar de CANCION
+ 
+);
+
+-- Crea la tabla CANCION
+CREATE TABLE CANCION(
+    numero INT NOT NULL,
+    titulo VARCHAR(80) NOT NULL,
+    id_pais INT NOT NULL,
+
+    CONSTRAINT PK_CANCION_numero PRIMARY KEY (numero),
+    CONSTRAINT UQ_CANCION_id_pais  UNIQUE (id_pais),
+    
+);
+
+-- Crea la tabla PAIS
+CREATE TABLE PAIS(
+    identificador INT NOT NULL,
+    denominacion VARCHAR(70) NOT NULL
+);
+
+-- Crea la tabla VOTACION
+CREATE TABLE VOTACION(
+    id_vota_a INT NOT NULL,
+    id_es_votado_por INT NOT NULL,
+    puntos INT NOT NULL,
+    CONSTRAINT PK_VOTACION PRIMARY KEY (id_vota_a,id_es_votado_por),
+    
+);
+
+
+ALTER TABLE INTERPRETE
+ADD
+CONSTRAINT FK_INTERPRETE_num_cancion FOREIGN KEY (num_cancion)
+                                    REFERENCES CANCION (numero);
+
+ALTER TABLE CANCION
+ADD
+CONSTRAINT FK_CANCION_id_pais FOREIGN KEY (id_pais) 
+                                REFERENCES PAIS (identificador);
+
+ALTER TABLE VOTACION
+ADD
+CONSTRAINT FK_PAIS_vota_a FOREIGN KEY (id_vota_a)
+                            REFERENCES PAIS (identificador),
+    CONSTRAINT FK_PAIS_es_votado_por FOREIGN KEY (id_es_votado_por)
+                            REFERENCES PAIS (identificador);
---
+++ creacion de tablas/exercicios_casa_practica/LIGA.sql
@@ -0,0 +1,74 @@
+IF EXISTS (SELECT * FROM sys.databases WHERE name = 'LIGA')
+BEGIN
+    DROP DATABASE LIGA;
+END
+GO
+
+
+CREATE DATABASE LIGA;
+GO
+USE LIGA;
+GO
+
+CREATE TABLE EQUIPO(
+    identificador INT NOT NULL,
+    nombre VARCHAR(30) NOT NULL,
+    cif CHAR(9) NOT NULL,  
+    presupuesto DECIMAL(14,2) NOT NULL, 
+    cod_division INT NOT NULL,
+    num_campo INT NOT NULL,
+
+    CONSTRAINT PK_EQUIPO_identificador PRIMARY KEY (identificador),
+    CONSTRAINT UQ_EQUIPO_nombre UNIQUE (nombre),
+    CONSTRAINT UQ_EQUIPO_cif UNIQUE (cif),
+    CONSTRAINT UQ_EQUIPO_num_campo UNIQUE (num_campo),
+);
+
+
+CREATE TABLE PARTIDO(
+    fecha_hora DATETIME NOT NULL,
+    observaciones VARCHAR(150) NULL,
+    id_equipo_local INT NOT NULL,
+    id_equipo_visitante INT NOT NULL,
+
+    CONSTRAINT PK_PARTIDO_equipo_local_fecha_hora PRIMARY KEY (fecha_hora,id_equipo_local)
+);
+
+CREATE TABLE CAMPO(
+    numero INT NOT NULL,
+    nombre VARCHAR(100) NOT NULL,
+    capacidad INT NOT NULL,
+
+    CONSTRAINT PK_CAMPO_numero PRIMARY KEY (numero),
+    CONSTRAINT UQ_CAMPO_nombre UNIQUE (nombre)
+)
+
+CREATE TABLE DIVISION(
+    codigo INT NOT NULL,
+    nombre VARCHAR(70) NOT NULL,
+    categoria CHAR(4) NULL,
+
+    CONSTRAINT PK_DIVISION_codigo PRIMARY KEY (codigo),
+    CONSTRAINT UQ_DIVISION_nombre UNIQUE (nombre)
+)
+
+--FK
+
+ALTER TABLE EQUIPO
+ADD
+CONSTRAINT FK_EQUIPO_cod_divisiono FOREIGN KEY (cod_division)
+                                    REFERENCES DIVISION (codigo)
+
+ALTER TABLE PARTIDO
+ADD
+CONSTRAINT FK_PARTIDO_id_equipo_local FOREIGN KEY (id_equipo_local)
+                                    REFERENCES PARTIDO (identificador)
+
+
+ALTER TABLE PARTIDO
+ADD
+CONSTRAINT FK_PARTIDO_id_equipo_visitante FOREIGN KEY (id_equipo_visitante)
+                                    REFERENCES PARTIDO (identificador)
+
+
+
